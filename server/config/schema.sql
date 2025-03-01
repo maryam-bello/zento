befb42fe69ef3bd1000f0b26793e925aa08ac587
Maryam <maryambello1967@gmail.com>
2025-03-01 17:34:00 +0100

    created database 'zento' and added tables. created a connection to the database.

---
+++ server/config/schema.sql
@@ -0,0 +1,20 @@
+USE zento;
+
+CREATE TABLE users (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tasks (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  category VARCHAR(255) DEFAULT 'To Do',
+  priority ENUM('low', 'medium', 'high') DEFAULT 'medium',
+  status ENUM('done', 'pending') DEFAULT 'pending',
+  due_date DATE DEFAULT (CURRENT_TIMESTAMP),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
