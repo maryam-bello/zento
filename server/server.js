2787070984828271cf2a7d41ad4d4afddea576fb
Maryam <maryambello1967@gmail.com>
2025-03-06 17:26:30 +0100

    implemented cors

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv').config({path: './config/.env'});
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
-
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/auth', authRoutes);
 
